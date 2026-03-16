b1153da5ff4a5c27bdcb893629a83c8ee6f39c2c
Glowman554 <vossjanick62@gmail.com>
2026-03-16 10:16:56 +0100

    automatically use latest preset version

--- website.sh
+++ website.sh
@@ -4,10 +4,19 @@ source tools/filething.sh
 
 UPLOAD_SERVER="https://filething.toxicfox.de"
 UPLOAD_AUTH_TOKEN="$MICROOS_UPLOAD_TOKEN"
+BASE=network
 
-INITRD=https://filething.toxicfox.de/files/384c795c-68f0-4d65-9ab9-4568f54c0a21.saf
-KERNEL=https://filething.toxicfox.de/files/034c0e85-e46c-400f-842a-465e55b50da4.elf
-SYMBOLS=https://filething.toxicfox.de/files/1a24effb-eb08-4f31-944b-16d5669b301a.syms
+response=$(curl -sS "https://toxicfox.de/api/v1/microos/info/$BASE")
+
+
+
+INITRD=$(echo "$response" | jq -r '.data.initrd')
+KERNEL=$(echo "$response" | jq -r '.data.kernel')
+SYMBOLS=$(echo "$response" | jq -r '.data.symbols')
+
+echo "INITRD: $INITRD"
+echo "KERNEL: $KERNEL"
+echo "SYMBOLS: $SYMBOLS"
 
 bash inject.sh $INITRD
 
